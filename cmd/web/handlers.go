fce108f617a150557226d12c5c69bdeae8de4008
vyct1 <thienvy95@gmail.com>
2025-05-05 09:10:04 +0700

    init proj

---
+++ cmd/web/handlers.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"path"
+)
+
+var pathToTemplate = "./templates/"
+
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
+	_ = app.render(w, r, "home.page.gohtml", &TemplateData{})
+}
+
+type TemplateData struct {
+	IP   string
+	Data map[string]any
+}
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, t string, data *TemplateData) error {
+	// parse the template from disk
+	parsedTemplate, err := template.ParseFiles(path.Join(pathToTemplate, t))
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return err
+	}
+
+	// execute the template, passing it date, if any
+	err = parsedTemplate.Execute(w, data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
