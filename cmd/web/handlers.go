c6dfae8dde155b538b27888b818cf9820edbea47
vyct1 <thienvy95@gmail.com>
2025-05-05 10:22:57 +0700

    finish section 5

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"path"
 )
@@ -25,10 +27,28 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, t string,
 		return err
 	}
 
+	data.IP = app.ipFromContext(r.Context())
+
 	// execute the template, passing it date, if any
 	err = parsedTemplate.Execute(w, data)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func (app *application) Login(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	email := r.Form.Get("email")
+	password := r.Form.Get("password")
+
+	log.Println(email, password)
+
+	fmt.Fprint(w, email)
 }
